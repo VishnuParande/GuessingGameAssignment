303117580c474cf7e9a4de79d9d080d921b27e98
VishnuP <vishnudas.parande@neurealm.com>
2025-08-20 15:17:35 -0500

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Function to count the number of files in the current directory
+count_files() {
+  local file_count=$(find . -maxdepth 1 -type f | wc -l)
+  echo "$file_count"
+}
+
+# Main function
+play_game() {
+  echo "Welcome to the guessing game!"
+  echo "Try to guess how many files are there in current directory!"
+
+  local count=$(count_files)
+  local guess_count=0
+  local user_guess
+
+  while true; do
+    read -p "Your guess: " user_guess
+    ((guess_count++))
+
+    # Compare the user's guess with the actual file count using numerical comparison operators.
+    if (( user_guess == secret_number )); then
+      echo "Congratulations! You guessed the correct number of files: $count, in $guess_count tries."
+      break
+    elif (( user_guess < secret_number )); then
+      echo "Guess is too low. try again."
+    else
+      echo "guess is too high. try again."
+    fi
+  done
+}
+
+play_game
